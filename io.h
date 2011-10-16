f83d940d9ec7034453eff9cf7010271b5a0923f6
Jon Craton <jncraton@gmail.com>
2011-10-16 19:08:34 -0400

    - added config.h to manage settings

--- io.h
+++ io.h
@@ -2,10 +2,7 @@
 #define IO_H
 
 #include <pic.h>
-
-#define LED 0b00000001 //GP0
-#define BAT_CON_A 0b00000100 //GP2
-#define BAT_CON_B 0b00100000 //GP5
+#include "config.h"
 
 void toggleLED();
 void enableChargeA();
--- main.c
+++ main.c
@@ -1,34 +1,10 @@
 #include <pic.h>
 
 #include "io.h"
+#include "adc.h"
 
 // Configure for internal oscillator at 4 MHz
 __CONFIG (INTIO & BORDIS & OSC_4MHZ & UNPROTECT & MCLREN & PWRTDIS & WDTDIS);
-
-// Set voltage to charge to
-// 209 = (4.1/5.0) * 255
-// Stop charging at 4.1 volts
-#define V_CUTOFF 209
-
-unsigned char getVoltage() {
-    for(char i = 0; i < 255; i++);
-    ADCON0 |= 0b00000010; // Start conversion
-    while(ADCON0 & 0b00000010); // Wait for conversion
-    unsigned char voltage = ADRESH;
-    if(voltage > V_CUTOFF) voltage = V_CUTOFF;
-    if(voltage < 120) voltage = V_CUTOFF; // Return high value if no battery present
-    return voltage;
-}
-
-unsigned char getVoltageA() {
-    ADCON0 = 0b00000101; // Start the ADC on AN1
-    return getVoltage();
-}
-
-unsigned char getVoltageB() {
-    ADCON0 = 0b00001101; // Start the ADC on AN3
-    return getVoltage();
-}
 
 void init() {
     CCP1CON = 0b00000000; // Disable Comparater and PWM
